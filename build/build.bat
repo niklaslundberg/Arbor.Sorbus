4f13e4a600014958fbd0897f28fdcaf5c8d9191e
Niklas Lundberg <info@nlundberg.se>
2018-07-15 23:11:53 +0200

    Fixed nuget restore in build script.

--- build/build.bat
+++ build/build.bat
@@ -1,9 +1,7 @@
 SET Arbor.X.Build.NetAssembly.PatchingEnabled=false
 SET Arbor.X.Tools.External.MSpec.Enabled=
-
-SET Version.Major=1
-SET Version.Minor=2
-SET Version.Patch=0
-SET Version.Build=1
+SET Arbor.X.MSBuild.NuGetRestore.Enabled=true
 
 CALL "%~dp0\Build.exe"
+
+EXIT /B %ERRORLEVEL%
