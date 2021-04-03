d3ffc1beaaa4effca705e0be5bda149318f48005
Niklas Lundberg <info@nlundberg.se>
2021-04-03 10:31:18 +0200

    Enable build on master.

--- build/build.bat
+++ build/build.bat
@@ -13,17 +13,24 @@ SET Arbor.Build.NetAssembly.Configuration=
 SET Arbor.Build.MSBuild.NuGetRestore.Enabled=true
 SET Arbor.Build.Tools.External.Xunit.NetCoreApp.Enabled=false
 SET Arbor.Build.BuildNumber.UnixEpochSecondsEnabled=true
+SET CI_BUILD=true
 SET Arbor.Build.Tools.External.SymbolServer.Enabled=true
 SET Arbor.Build.Tools.External.VisualStudio.Version.PreRelease.Enabled=true
 SET Arbor.Build.Tools.External.MSBuild.AllowPrerelease.Enabled=true
 SET Arbor.Build.Log.Level=Debug
 SET Arbor.Build.Tools.External.MSBuild.DotNet.Enabled=true
+SET Arbor.Build.Vcs.Branch.BranchModel=GitFlowBuildOnMain
+
 SET Fallback.Version.Build=0
+
+IF "%Arbor.Build.Bootstrapper.AllowPrerelease%" == "" (
+	SET Arbor.Build.Bootstrapper.AllowPrerelease=true
+)
+
 SET Arbor.Build.NuGet.ReinstallArborPackageEnabled=true
 SET Arbor.Build.NuGet.VersionUpdateEnabled=false
 SET Arbor.Build.Artifacts.PdbArtifacts.Enabled=true
 SET Arbor.Build.NuGet.Package.CreateNuGetWebPackages.Enabled=true
-SET Arbor.Build.NuGet.PackageUpload.PackageExcludeStartsWithPatterns=dotnet-
 CALL dotnet arbor-build
 
 EXIT /B %ERRORLEVEL%
